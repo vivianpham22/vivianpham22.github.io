ef428dacd8a4b59a56870e72ff92af2da0c43dbc
unknown <you@example.com>
2017-10-18 17:43:07 -0400

    initial commit

---
+++ Scripts/app.js
@@ -0,0 +1,15 @@
+﻿// Portfolio Slider
+
+$(document).ready(function () {
+    $('.flexslider').flexslider({
+        animation: "slide",
+        directionNav: false,
+        controlNav: true,
+        touch: false,
+        pauseOnHover: true,
+        //start: function () {
+        //    $.waypoints('refresh');
+        //}
+    });
+})
+
